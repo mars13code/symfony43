58304dcb545b8f60277a456e86bf170c78e1edb5
formation8 <formation8@applh.com>
2019-06-10 12:11:32 +0000

    ajout test unitaire create Contenu

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -2,12 +2,74 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class BasicTest extends TestCase
+use App\Entity\Contenu;
+use App\Repository\ContenuRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class BasicTest extends KernelTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null; // avoid memory leaks
+    }
+    
     public function testSomething()
     {
         $this->assertTrue(true);
+    }
+
+    public function testContenuPersist()
+    {
+        // https://symfony.com/doc/current/testing/doctrine.html
+        
+        // nbContenu avant insertion
+        $contenuRepository = $this->entityManager->getRepository(Contenu::class);
+        $nbContenu = $contenuRepository->count(["categorie" => "blog"]);
+
+        // création nouveau contenu
+        $contenu = new Contenu;
+        
+        $contenu->setTitre("titre ".date("H-i-s Ymd"));
+        $contenu->setUri("uri-".date("H-i-s-Ymd"));
+        $contenu->setCode("code ".date("H-i-s-Ymd"));
+        $contenu->setCategorie("blog");
+        $contenu->setImageSrc("assets/images/test.jpg");
+        $contenu->setDateCreation(new \DateTime);
+        
+        // persistence 
+        $this->entityManager->persist($contenu);
+        $this->entityManager->flush();
+
+        // vérification: nbContenu2 après insertion
+        $nbContenu2 = $contenuRepository->count(["categorie" => "blog"]);
+        
+        // vérification qu'un entité Contenu est bien ajoutée
+        $this->assertEquals(1, $nbContenu2 - $nbContenu);
+        
     }
 }
